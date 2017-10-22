fd11ff7f77e3eca0a6a288b31e5f46eb4f5df14c
Thinh Vu <thinh.vu@ge.com>
2017-10-22 15:03:02 -0700

    Added contract information.

--- event-server/server.js
+++ event-server/server.js
@@ -10,87 +10,97 @@ const web3 = new Web3(web3Provider);
 
 const PollContractABI = require('../solidityFiles/Poll.json');
 const TruffleContract = require('truffle-contract');
-const DEFAULT_GAS = 4000000
+const DEFAULT_GAS = 4000000;
 
-let PollContract = TruffleContract(PollContractABI)
-PollContract.setProvider(web3Provider)
+let PollContract = TruffleContract(PollContractABI);
+PollContract.setProvider(web3Provider);
 
-console.log(PollContract)
-
-// web3.eth.net.getNetworkType()
-// .then(console.log);
-//
-// web3.eth.net.isListening()
-// .then(console.log)
-
-app.post('/createPoll', (req, res) => {
-  //createPoll("test", "test2", 10, 2, 1, {from: "0xcd15fea156c1c9d10e5b912b86a453d910c25df9"})
-  _logPollInfo("0x34e5a6371897510bc2233114157579d8c1a6eb57")
-  .then((address) => {
-    res.send(address);
-  }).catch(function () {
-     console.log("Promise Rejected1");
-});
-})
-
-function createPoll (title, proposition, endTime, votesRequired, votesAllowed, msgSender)  {
-    console.log(`Create poll: ${title}, ${proposition}, ${endTime}, ${votesRequired}, ${votesAllowed}`)
+function createPoll (title, proposition, endTime, votesRequired, votesAllowed, msgSender) {
     let myInstance;
-    return PollContract.new(title, proposition, endTime, votesRequired, votesAllowed,
-      {from: msgSender, gas: DEFAULT_GAS})
+    
+    return PollContract.new(
+        title, 
+        proposition, 
+        endTime, 
+        votesRequired, 
+        votesAllowed, 
+        { from: msgSender, gas: DEFAULT_GAS })
     .then((instance) => {
         console.log("Instance Address: " + instance.address)
-        logPollInfo(instance.address);
+        _logPollInfo(instance.address);
         return instance.address;
     })
     .catch(function (error) {
-      console.trace("THIS THE ERROR: " + error);
-     console.log("Promise Rejected2");
-   });
+        console.trace("THIS THE ERROR: " + error);
+        console.log("Promise Rejected2");
+    });
+};
+
+function _logPollInfo (contractAddress) {
+    return PollContract.at(contractAddress)._title.call()
+    .then((title) => {
+        console.log(title)
+        return PollContract.at(contractAddress)._proposition.call()
+    })
+    .then((proposition) => {
+        console.log(proposition);
+        return PollContract.at(contractAddress)._endTime.call()
+    })
+    .then((endTime) => {
+        console.log(endTime.toNumber())
+        return PollContract.at(contractAddress)._votesRequired.call()
+    })
+    .then((votesRequired) => {
+        console.log(votesRequired.toNumber())
+        return PollContract.at(contractAddress)._votesAllowed.call()
+    })
+    .then((votesAllowed) => {
+        console.log(votesAllowed.toNumber())
+    })
 }
 
- function _logPollInfo (contractAddress) {
-  return PollContract.at(contractAddress)._title.call()
-  .then((title) => {
-    console.log(title)
-    return PollContract.at(contractAddress)._proposition.call()
-  })
-  .then((proposition) => {
-    console.log(proposition);
-    return PollContract.at(contractAddress)._endTime.call()
-  })
-  .then((endTime) => {
-    console.log(endTime)
-    return PollContract.at(contractAddress)._votesRequired.call()
-  })
-  .then((votesRequired) => {
-    console.log(votesRequired)
-    return PollContract.at(contractAddress)._votesAllowed.call()
-  })
-  .then((votesAllowed) => {
-    console.log(votesAllowed)
-  })
-}
+let pollAddress;
 
-
-
+app.post('/createPoll', (req, res) => {
+    createPoll('Boba', 'Should we get boba?', 1508691600, 6, 1, '0xa6da6bc67b3e9d9b2d70f277e43cd9f668c93234')
+    .then((address) => {
+        pollAddress = address;
+        res.send(address);
+    }).catch(function (err) {
+        console.error("The promise was rejected.");
+        console.trace(err);
+    });
+});
+    
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
-const options = {};
+const voters = [];
+
+const votes = {
+    "yes": 0,
+    "no": 0,
+    "voters": voters
+};
 
 app.get('/newAccount', (req, res) => {
-  const responseObject = web3.eth.accounts.create()
-  res.send(responseObject)
+    const responseObject = web3.eth.accounts.create()
+    res.send(responseObject)
 });
 
+eventEmitter.on('vote', (voter, vote) => {
+    switch(vote) {
+        case 0: {
+            votes.yes += 1;
+            break;
+        }
 
-eventEmitter.on('vote', (option, vote) => {
-    if (options[option]) {
-        options[option] += 1;
-    } else {
-        options[option] = 1;
+        case 1: {
+            votes.no += 1;
+        }
     }
+
+    voters.push(voter);
 });
 
 const WebSocket = require('ws');
@@ -100,57 +110,64 @@ wss.on('connection', ws => {
     ws.on('message', message => {
         console.log('Received: %s', message);
     });
-
+    
     ws.send('Connection opened');
 });
 
-let subscription = '';
-
-
+let pollInstance;
 
 app.get('/votes', (req, res) => {
-    subscription = web3.eth.subscribe('logs', {
-        address: req.query.address,
-        topics: ['vote']
-    }, (error, result) => {
-        if (error) {
-            console.error(`Error subscribing to address, ${req.query.address}, ${error}`);
-            res.status(404).end();
-        } else {
-            res.send(`Successfully subscribed to logs from ${subscription.id}`);
-        }
+    PollContract.at(pollAddress).then(instance => {
+        pollInstance = instance;
+        res.send(`Successfully deployed poll.`);
     });
 });
 
-// subscription.on('data', (result) => {
-//     eventEmitter.emit('vote', result.option, result.vote);
-// });
-
-app.post('/votes', (req, res) => {
-    subscription.unsubscribe((error, success) => {
+if (pollInstance) {
+    let transactions = pollInstance.Voted({fromBlock: "latest"});
+    transactions.watch((error, result) => {
         if (error) {
-            console.error(`Error unsubscribing from ${subscription.id}`);
-            res.status(404).end();
+            console.error(error);
         } else {
-            wss.broadcast = () => {
-                wss.clients.forEach(client => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(options);
-                    }
-                });
-            };
-
-            wss.broadcast = () => {
-                wss.clients.forEach(client => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.close();
-                    }
-                });
-            };
-
-            res.send(`Successfully unsubscribed from ${subscription.id}`);
+            eventEmitter.emit('vote', result.voterAddress, result.voteType);
         }
     });
+};
+
+// if (subscription) {
+//     subscription.on('data', (result) => {
+//         eventEmitter.emit('vote', result.option, result.vote);
+//     });
+// };
+
+app.post('/vote', (err, req, res) => {
+    PollContract.at(pollAddress).then(instance => {
+        instance.vote(req.body.voteType, { from: req.body.account, gas: DEFAULT_GAS });
+        res.status(200).send('Successfully voted.');
+    }).catch(error => {
+        console.error(error);
+        res.status(404).send('Could not vote.');
+    });
+});
+
+app.delete('/votes', (req, res) => {
+        wss.broadcast = () => {
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(votes);
+                }
+            });
+        };
+        
+        wss.broadcast = () => {
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.close();
+                }
+            });
+        };
+        
+        res.send(`Successfully unsubscribed from ${subscription.id}`);
 });
 
 process.on('exit', () =>{
@@ -160,3 +177,4 @@ process.on('exit', () =>{
 const server = app.listen(3000, () => {
     console.log(`App is listening on port ${server.address().port}...`);
 });
+    
